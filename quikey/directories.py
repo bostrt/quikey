55f8bad8f394515e689a726a6e5bdb7023182b75
Robert Bost <bostrt@gmail.com>
2019-10-16 21:43:11 -0600

    Further correcting for new XDG lib

--- quikey/directories.py
+++ quikey/directories.py
@@ -3,9 +3,9 @@ import os
 
 class AppDirectories():
     def __init__(self):
-        self.data = os.fspath(BaseDirectory.xdg_data_home.joinpath('quikey/'))
-        self.config = os.fspath(BaseDirectory.xdg_config_home.joinpath('quikey/'))
-        self.cache = os.fspath(BaseDirectory.xdg_cache_home.joinpath('quikey/'))
+        self.data = os.fspath(os.path.join(BaseDirectory.xdg_data_home, 'quikey/'))
+        self.config = os.fspath(os.path.join(BaseDirectory.xdg_config_home, 'quikey/'))
+        self.cache = os.fspath(os.path.join(BaseDirectory.xdg_cache_home, 'quikey/'))
         os.makedirs(self.data, exist_ok=True)
         os.makedirs(self.config, exist_ok=True)    
         os.makedirs(self.cache, exist_ok=True)
