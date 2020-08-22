5d4b66ab33323019c286e320c6c8f79c4fc975b1
Robert Bost <rbost@redhat.com>
2020-08-22 17:44:51 -0600

    Filled out more help information. Adding support for -h for help.

--- quikey/quikey.py
+++ quikey/quikey.py
@@ -21,17 +21,23 @@ MARKER = '''
 # Insert your quikey phrase into this file then save and close.
 '''
 
+CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+
 def get_database():
     appDirs = AppDirectories() # XDG folders
     d = Database(appDirs)
     return d
 
-@click.group()
+@click.group(context_settings=CONTEXT_SETTINGS)
 @click.pass_context
 def cli(ctx):
+    """A keyboard macro tool.
+    
+    Features, Feedback, and Bugs: https://github.com/bostrt/quikey
+    """
     ctx.obj = {'database': get_database()}
     
-@cli.command()
+@cli.command(help='Add a new phrase')
 @click.option('--name' ,'-n', required=True, help='Name of quikey phrase to add.')
 @click.option('--tag', '-t', multiple=True, help='Optional tags for the phrase. You can specify this option multiple times.')
 @click.option('--phrase', '-p', help='The full phrase to add. If this option is not specified then your default editor ($EDITOR) will be used.')
@@ -58,7 +64,7 @@ def add(ctx,name,phrase,tag):
     db.put(name, contents, tag)
     click.echo('quikey phrase with key of %s added.' % name)
 
-@cli.command()
+@cli.command(help='Edit an existing phrase')
 @click.option('--name', '-n', help='Name of quikey phrase to edit.')
 @click.pass_context
 def edit(ctx,name):
@@ -83,7 +89,7 @@ def edit(ctx,name):
         click.echo('quikey phrase with key of %s does not exist.' % name)
         
 
-@cli.command()
+@cli.command(help='Remove a phrase')
 @click.option('--name', '-n', help='Name of quikey phrase to remove.')
 @click.pass_context
 def rm(ctx,name):
@@ -101,7 +107,7 @@ def rm(ctx,name):
     else:
         click.echo('quikey phrase with key of %s does not exist.' % name)
     
-@cli.command()
+@cli.command(help='List all phrases')
 @click.option('--show-all', is_flag=True, help='Show the entire quikey phrase instead of a shortened version '
                                                'for long quikey phrases.')
 @click.pass_context
@@ -117,7 +123,7 @@ def ls(ctx, show_all):
     output = AsciiTable(table)
     click.echo(output.table)
 
-@cli.command()
+@cli.command(help='Import from AutoKey')
 @click.option('--location', '-l', default=BaseDirectory.xdg_config_home+"/autokey/data/", show_default=True, help='Location of top level directory to import from autokey')
 @click.pass_context
 def keyimport(ctx,location):
@@ -146,11 +152,11 @@ def keyimport(ctx,location):
         db.put(key, contents, tags)
         click.echo('quikey phrase with key of %s added.' % key)
 
-@cli.command()
+@cli.command(help='Display version')
 def version():
     click.echo("quikey %s" % __version__)
 
-@cli.group()
+@cli.group(help='Configure autostart of quikey')
 def autostart():
     pass
 
@@ -164,16 +170,16 @@ def disable():
     "Disable autostart at login for quikey."
     disableAutostart()
 
-@cli.command()
+@cli.command(help='Display status of quikey daemon')
 @click.pass_context
 def status(ctx):
     subprocess.run(['quikey-daemon', 'status'])
     click.echo("Database location: " + ctx.obj['database'].dbFile)
 
-@cli.command()
+@cli.command(help='Start quikey daemon')
 def start():
     subprocess.run(['quikey-daemon', 'start'])
 
-@cli.command()
+@cli.command(help='Stop quikey daemon')
 def stop():
     subprocess.run(['quikey-daemon', 'stop'])
